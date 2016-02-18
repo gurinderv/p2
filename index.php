9bb749a50cbf30c6a3c3aa5e6661fb491fb08e00
Gurinder Virdi <gurinder@gurindervirdi.com>
2016-02-17 19:11:48 -0800

    1st loop done

--- index.php
+++ index.php
@@ -6,11 +6,36 @@
 <?php require('logic.php'); ?>
 <title>Password Generator - P2</title>
 <meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="style.css">
 
 </head>
 
 <body>
+<!--
+1. input for how many words to use
+2. whether to include a number
+3. whether to include a special symbol (like @)
+4. basic server-side validation
 
+Optional:
+1. more configs: how many special chars, different special chars, max length, all upper, lower, first letter upper, etc
+2. scrape an online word list to build really long strings
+-->
+
+<h1>xkcd Password Generator</h1>
+<p>
+<!--  Your password is:  -->
+</p>
+
+<form action='index.php' method="post">
+  <fieldset>
+    <legend class="generateOptions">Password Options</legend>
+  <label># of Words<input type="text" name="numWords">(Max 5)</input><br/>
+  <label><input type="checkbox" name="addNumber">Add a Number</input><br/>
+  <label><input type="checkbox" name="Add a symbol">Add a Symbol</input><p>
+  <button type="submit" name="generate">Generate Password!</button>
+</fieldset>
+</form>
 
 </body>
 
---
+++ logic.php
@@ -0,0 +1,14 @@
+<?php
+
+$words = ['test','bank','there','their','home','computer','language','file','list','generate','general','list','bring','keyboard','laptop','word'];
+
+#use the number of words as the repeats in the for loop
+#server side validations - number of words > 4, number in numwords field
+
+for($i = $_POST['numWords']; $i > 0; $i--){
+  echo "word";
+
+}
+#in the for loop if symbols is checked then concat with a hyphen
+#in the for loop if numbers is checked then randomly add 1-(numWords-2)
+?>
