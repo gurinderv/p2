3d9e12deab66200070267c557f60f14feb485de0
Gurinder Virdi <gurinder@gurindervirdi.com>
2016-02-17 20:56:51 -0800

    removed comments

--- index.php
+++ index.php
@@ -11,16 +11,6 @@
 </head>
 
 <body>
-<!--
-1. input for how many words to use
-2. whether to include a number
-3. whether to include a special symbol (like @)
-4. basic server-side validation
-
-Optional:
-1. more configs: how many special chars, different special chars, max length, all upper, lower, first letter upper, etc
-2. scrape an online word list to build really long strings
--->
 
 <h1>xkcd Password Generator</h1>
 <p>
